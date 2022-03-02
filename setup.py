f41f65f892ae6373cb7b00c10ee441b55ea05673
Will Breaden Madden <wbm@protonmail.ch>
2022-03-02 04:57:53 +0000

    update

--- setup.py
+++ setup.py
@@ -7,27 +7,28 @@ import setuptools
 
 def main():
     setuptools.setup(
-        name             = 'wirereport',
-        version          = '2022.03.02.0432',
-        description      = 'a small text editor',
-        long_description = long_description(),
-        url              = 'https://github.com/wdbm/report',
-        author           = 'Will Breaden Madden',
-        author_email     = 'wbm@protonmail.ch',
-        license          = 'GPLv3',
-        py_modules       = [
-                           'report'
-                           ],
-        install_requires = [
-                           'docopt'
-                           ],
-        scripts          = [
-                           'report.py'
-                           ],
-        entry_points     = '''
-                           [console_scripts]
-                           report = report:report
-                           '''
+        name                 = 'wirereport',
+        version              = '2022.03.02.0432',
+        description          = 'a small text editor',
+        long_description     = long_description(),
+        url                  = 'https://github.com/wdbm/report',
+        author               = 'Will Breaden Madden',
+        author_email         = 'wbm@protonmail.ch',
+        license              = 'GPLv3',
+        py_modules           = [
+                               'report'
+                               ],
+        install_requires     = [
+                               'docopt'
+                               ],
+        scripts              = [
+                               'report.py'
+                               ],
+        entry_points         = {
+                               'console_scripts': ('report=report:report')
+                               },
+        include_package_data = True,
+        package_data         = {'': ['cmtex9.ttf']}
     )
 
 def long_description(filename='README.md'):
